5b43f951d355ff94cfcd589784b989b689b43200
Dangh <54danghao@gmail.com>
2019-08-26 21:05:09 +0800

    统计热门商品Top3——从Kafka消费

--- HotItemsAnalysis/src/main/scala/analysis/applicaotion/KafkaProducer.scala
+++ HotItemsAnalysis/src/main/scala/analysis/applicaotion/KafkaProducer.scala
@@ -1,15 +1,28 @@
 package analysis.applicaotion
 
+import analysis.utils.MyUtils
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
+import scala.io.BufferedSource
+
 object KafkaProducer {
-
-	def writeToKafka(topic: String): Unit = {
-
-	}
-
 
 	//从文件读取数据存入kafka，供消费者消费
 	def main(args: Array[String]): Unit = {
-		writeToKafka("hotItems")
+		writeFileToKafka("hotItems")
 	}
 
+	def writeFileToKafka(topic: String): Unit = {
+		val producer = new KafkaProducer[String, String](MyUtils.getKafkaProperties)
+
+//		val inputBS: BufferedSource = io.Source.fromFile("F:\\workSpace\\DataHouse_RealTime_Flink\\HotItemsAnalysis\\src\\main\\resources\\testKafka.csv")
+		val inputBS: BufferedSource = io.Source.fromFile("F:\\workSpace\\DataHouse_RealTime_Flink\\HotItemsAnalysis\\src\\main\\resources\\UserBehavior.csv")
+
+		for (line <- inputBS.getLines()) {
+			val record = new ProducerRecord[String, String](topic, line)
+			producer.send(record)
+		}
+
+		producer.close()
+	}
 }
